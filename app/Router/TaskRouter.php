beeb6148ecc5dd97427ffee81d94e6d0763088bb
HarkuLi <harku85697708@hotmail.com>
2018-01-05 17:55:53 +0800

    rename the file

--- app/Router/TaskRouter.php
+++ app/Router/TaskRouter.php
@@ -8,7 +8,15 @@ class TaskRouter extends SubRouter
     public function setUpRouter(): void
     {
         $this->get("/", function () {
-            require __DIR__."/../Controller/Task/showTaskPage.php";
+            require __DIR__."/../Controller/Task/taskPage.php";
+        });
+
+        $this->get("/new", function () {
+            require __DIR__."/../Controller/Task/taskNew.php";
+        });
+
+        $this->post("/create", function () {
+            require __DIR__."/../Controller/Task/taskCreate.php";
         });
 
         $this->get("/test", function () {
